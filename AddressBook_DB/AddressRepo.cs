232f70232a5f249bdacb4dd7131c143b0061a265
Imran2526 <imraninfo97@gmail.com>
2020-12-30 15:06:20 +0530

    [Imran] Add: Welcome To Address Book With ADO

---
+++ AddressBook_DB/AddressRepo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace AddressBook_DB
+{
+    class AddressRepo
+    {
+        public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AddressBook;Integrated Security=True";
+        SqlConnection Connection = new SqlConnection(connectionString);
+
+        public void CheckConnection()
+        {
+            try
+            {
+                this.Connection.Open();
+                Console.WriteLine("Connection Established");
+                this.Connection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
+
---
+++ AddressBook_DB/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AddressBook_DB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("************Welcome To AddRessBook DB************");
+            AddressRepo Repo = new AddressRepo();
+            Repo.CheckConnection();
+        }
+    }
+}
