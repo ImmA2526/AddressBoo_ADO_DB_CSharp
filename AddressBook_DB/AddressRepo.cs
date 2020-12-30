8aa9d80f1272084bb1653d4b5716ff948e9e35b4
Imran2526 <imraninfo97@gmail.com>
2020-12-30 16:23:19 +0530

    [Imran] Add: Add Record in Data base

--- AddressBook_DB/AddressRepo.cs
+++ AddressBook_DB/AddressRepo.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Data.SqlClient;
-
+using System.Data;
 namespace AddressBook_DB
 {
     class AddressRepo
@@ -10,6 +10,9 @@ namespace AddressBook_DB
         public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AddressBook;Integrated Security=True";
         SqlConnection Connection = new SqlConnection(connectionString);
 
+        /// <summary>
+        /// UC1 Check Connection
+        /// </summary>
         public void CheckConnection()
         {
             try
@@ -21,6 +24,44 @@ namespace AddressBook_DB
             catch (Exception e)
             {
                 Console.WriteLine(e.StackTrace);
+            }
+        }
+
+        /// <summary>
+        /// UC 2 Add Record
+        /// </summary>
+        /// <param name="Model"></param>
+        /// <returns></returns>
+        public bool AddContacts(AddressModel Model)
+        {
+            try
+            {
+                using (this.Connection)
+                {
+                    SqlCommand CMD = new SqlCommand("SpAdd_Address", this.Connection);
+                    CMD.CommandType = CommandType.StoredProcedure;
+                    CMD.Parameters.AddWithValue("@firstName", Model.firstName);
+                    CMD.Parameters.AddWithValue("@lastName", Model.lastName);
+                    CMD.Parameters.AddWithValue("@address", Model.address);
+                    CMD.Parameters.AddWithValue("@city", Model.city);
+                    CMD.Parameters.AddWithValue("@state", Model.state);
+                    CMD.Parameters.AddWithValue("@zip", Model.zip);
+                    CMD.Parameters.AddWithValue("@phoneNumber", Model.phoneNumber);
+                    CMD.Parameters.AddWithValue("@BookName", Model.BookType);
+                    CMD.Parameters.AddWithValue("@BookType", Model.BookName);
+                    this.Connection.Open();
+                    var result = CMD.ExecuteNonQuery();
+                    this.Connection.Close();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
             }
         }
     }
--- AddressBook_DB/Program.cs
+++ AddressBook_DB/Program.cs
@@ -9,6 +9,19 @@ namespace AddressBook_DB
             Console.WriteLine("************Welcome To AddRessBook DB************");
             AddressRepo Repo = new AddressRepo();
             Repo.CheckConnection();
+            AddressModel Add = new AddressModel();
+            Add.firstName = "Imran";
+            Add.lastName = "Shaikh";
+            Add.address="Dighi";
+            Add.city="Pune";
+            Add.state="Maha";
+            Add.zip="456789";
+            Add.phoneNumber="9876543212";
+            Add.BookName="Family";
+            Add.BookType = "Friend";
+            Repo.AddContacts(Add);
+            Console.WriteLine("**********Insrted Recod**********");
+            Console.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},{8}",Add.firstName,Add.lastName,Add.address,Add.city,Add.state,Add.zip,Add.phoneNumber,Add.BookName,Add.BookType);
         }
     }
 }
